de154394240d1e4588fb7e7ce2f29cf8da590df4
Ernad Husremovic <hernad@bring.out.ba>
2023-06-03 16:15:21 +0200

    hernad activated

--- hosts/hped800g3/optin-persistence.nix
+++ hosts/hped800g3/optin-persistence.nix
@@ -24,7 +24,7 @@
     let
       mkHomePersist = user: lib.optionalString user.createHome ''
         mkdir -p /persist/${user.home}
-        #chown ${user.name} /persist/${user.home}
+        chown ${user.name}:${user.group} /persist/${user.home}
         chmod ${user.homeMode} /persist/${user.home}
       '';
       users = lib.attrValues config.users.users;
