b90965d7c20f86a319a29e26b90ce5e8405e44a7
Ernad Husremovic <hernad@bring.out.ba>
2023-06-09 11:04:31 +0200

    show iommu

---
+++ hosts/common/global/libvirtd.nix
@@ -0,0 +1,27 @@
+{config, pkgs, ... }:
+
+{
+
+  environment.systemPackages = with pkgs; [
+    #virtmanager
+    qemu
+    OVMF
+  ];
+
+   # https://gist.github.com/techhazard/1be07805081a4d7a51c527e452b87b26
+
+   #boot.kernelParams = [ "intel_iommu=on" ];
+
+   boot.kernelModules = [ 
+    "kvm-intel"
+    "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" 
+   ];
+   virtualisation.libvirtd.enable = true;
+   
+   #virtualisation.libvirtd.qemuVerbatimConfig = ''
+   # nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
+   #'';
+
+   # https://github.com/WhittlesJr/nixos-shared/blob/master/pciPassthrough.nix
+
+}
